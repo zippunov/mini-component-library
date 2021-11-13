cea0508f31ec3976f255ce636924b5e27283b996
Slava Zipp <zipp.pnl@gmail.com>
2021-11-13 09:18:47 +0200

    Select Companent implementation

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,60 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <NativeSelect value={value} onChange={onChange}>
+        {children}
+      </NativeSelect>
+      <StyledSelect>
+        {displayedValue}
+        <SelectIcon id="chevron-down" size="20" strokeWidth="3"/>
+      </StyledSelect>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  position: relative;
+  display: inline-block;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const StyledSelect = styled.div`
+  pointer-events: none;
+  position: relative;
+  background-color: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  white-space: nowrap;
+  width: min-content;
+  padding:12px 52px 12px 16px;
+  font-family: "Roboto", sans-serif;
+  font-weight: 400;
+  color: ${COLORS.gray700};
+
+  ${NativeSelect}:hover + & {
+    color: ${COLORS.black};
+  }
+
+  ${NativeSelect}:focus + & {
+    outline: 5px auto Highlight;
+    outline: 5px auto -webkit-focus-ring-color;
+  }
+`;
+
+const SelectIcon = styled(Icon)`
+  position: absolute;
+  top: 11px;
+  right: 16px;
+`;
 
 export default Select;
